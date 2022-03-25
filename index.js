3eda1b4d1a0e2221b1271b8d6753ebe58dfd2451
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-03-25 14:46:54 +0900

    web2-5 Static ServerPage

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const http = require('http');
+const fs = require("fs")
 
 const app = http.createServer(function (req,res){
+    let url = req.url;
+
+    if(url === '/')
+        url = '/index.html';
+    if(url === '/favicon.ico')
+        return res.writeHead(404);
+
     res.writeHead(200);
-    console.log(__dirname + req.url);
-    res.end("Hello World");
+    //console.log(__dirname + req.url);
+    res.end(fs.readFileSync(__dirname + url));
 
 });
 
-app.listen(80);
+app.listen(3333);
