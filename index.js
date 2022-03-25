ca82923754dffd3e9c36c056309e6a0247f2ff5e
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-03-25 15:06:27 +0900

    web2 간단한 동적 서버페이지

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const http = require('http');
-const fs = require("fs")
+const fs = require("fs");
+const url = require('url');
 
 const app = http.createServer(function (req,res){
-    let url = req.url;
-
-    if(url === '/')
-        url = '/index.html';
-    if(url === '/favicon.ico')
-        return res.writeHead(404);
-
-    res.writeHead(200);
-    //console.log(__dirname + req.url);
-    res.end(fs.readFileSync(__dirname + url));
+    let _url = req.url;
+    let queryData = url.parse(_url,true).query;
+    res.end(queryData.id);
 
 });
 
