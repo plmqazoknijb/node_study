7405b4fa6bd9d839462ce57a358834030f1f36f7
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-04-15 14:56:14 +0900

    web 2-36 글수정 - 파일명변경(게시글제목), 변경내용 저장

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-const http = require('http')
-const fs = require('fs')
-const url = require('url')
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
 
-function templateList(filelist) {
+function templateList(filelist){
     let list = '<ul>';
-    for(let i = 0; i < filelist.length; i++){
-        list += `<li> <a href="/?id=${filelist[i]}"> ${filelist[i]} </a> </li>`
+    for(let i=0; i<filelist.length; i++) {
+        list += `<li> <a href="/?id=${filelist[i]}"> ${filelist[i]} </a> </li>`;
     }
-    list += '</ul>'
+    list += '</ul>';
     return list;
 }
-
-function templateHTML(title,list,body){
-    return`    <!doctype html>
-                <html lang="ko">
-                <head>
-                  <title>WEB1 - ${title}</title>
-                  <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                <h2>${title}</h2>
-                <a href="/create">create</a>
-                <p>${body}</p>
-                </body>
-                </html>`
+function templateHTML(title, list, body,control){
+    return `
+          <!doctype html>
+          <html lang="ko">
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            ${control}
+            <p>${body}</p>
+          </body>
+          </html>
+          `
 }
-
 const app = http.createServer(function (request, response) {
     const _url = request.url
     const queryData = url.parse(_url, true).query
@@ -36,48 +37,96 @@ const app = http.createServer(function (request, response) {
         if (queryData.id === undefined) {
             const title = 'Welcome'
             const description = 'Hello, Node.js'
-            fs.readdir('data/',function (err,data){
+
+            fs.readdir('data/', function (err, data){
                 const list = templateList(data);
-                const template = templateHTML(title,list,description)
+                //메인화면에서는 create(새 게시글 작성)만 가능하게
+                const template = templateHTML(title, list, description,'<a href="create">create</a>');
                 response.writeHead(200)
                 response.end(template)
             })
-
-
         }else {
-            fs.readdir('data/',function (err,data){
-                let list = '<ul>';
-                for(let i = 0; i < data.length; i++){
-                    list += `<li> <a href="/?id=${data[i]}"> ${data[i]} </a> </li>`
-                }
-                list += '</ul>'
-                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+            fs.readdir('data/', function (err, data){
+                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description){
                     const title = queryData.id
                     const list = templateList(data);
-                    const template = templateHTML(title,list,description);
+                    const template = templateHTML(title,list,description,`<a href="create">create</a> <a href="update?id=${title}">update</a>`);
                     response.writeHead(200)
                     response.end(template)
                 })
-
             });
-
-
         }
     }else if(pathname === '/create'){
-        fs.readdir('data',function (err,data){
-           const title = 'Web - create';
-           const list = templateList(data);
-           const template = templateHTML(title,list,`
-<form action="create_process" method = "post">           
-    <p><input type="text" name="tilte" placeholder="title"> </p>
-    <p><textarea name="description" placeholder="description"></textarea> </p>
-    <p><input type="submit"></p>
-</form>`)
+        fs.readdir('data/',function (err,data){
+            const title = 'Web - create';
+            const list = templateList(data);
+            const template = templateHTML(title, list,`
+          <form action="create_process" method="post">
+              <p><input type="text" name="title" placeholder="title"></p>
+              <p><textarea name="description" placeholder="description"></textarea></p>
+              <p><input type="submit"></p>
+          </form>`, '') //글 생성 중에는 create가 안보이게
             response.writeHead(200);
             response.end(template)
         });
+    }
+    else if(pathname === '/create_process'){
+        let body = '';
+        request.on('data',function (data){
+            body += body + data;
+        });
+        request.on('end',function (){
+            const post = qs.parse(body);
+            const title = post.title;
+            const description = post.description;
+            fs.writeFile(`data/${title}`, description,"utf-8", function (err){
+                response.writeHead(302,{Location:`/?id=${title}`});
+                response.end();
+            })
+        });
+    }else if(pathname === '/update'){
+        //data : 실제 파일리스트 문자열의 배열 (파일이름은 게시글의 제목)
+        fs.readdir('data/',function (err,data){
+            //description : 파일안의 내용물(게시물의 내용)
+            fs.readFile(`data/${queryData.id}`,'utf-8',function (err,description) {
+                const title = queryData.id;
+                const list = templateList(data);
+                const template = templateHTML(title, list,`
+          <form action="update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p><textarea name="description" placeholder="description">${description}</textarea></p>
+              <p><input type="submit"></p>
+          </form>`, `<a href="/create">create  </a> <a href="/update?id=${title}">update</a>`) //글 생성 중에는 create가 안보이게
+                response.writeHead(200);
+                response.end(template)
+            });
 
-    }else {
+        });
+
+    } else if(pathname === '/update_process'){
+        let body = '';
+        request.on('data',function (data){
+            body += body + data;
+        });
+        request.on('end',function (){
+            const post = qs.parse(body);
+            const id = post.id;          //바꾸기전의 파일이름(게시물 제목)
+            const title = post.title;    //바꾼 이후의 파일이름(게시물 제목)
+            const description = post.description;
+            fs.rename(`data/${id}`,`data/${title}`,function (err){
+               response.writeHead(302,{Location:`/?id=${title}`},function (err){
+               })
+            });
+
+            fs.writeFile(`data/${title}`, description,"utf-8", function (err){
+                response.writeHead(302,{Location:`/?id=${title}`},function (err) {
+                    
+                });
+                response.end();
+            })
+        });
+    } else {
         response.writeHead(404)
         response.end('Not found')
     }
